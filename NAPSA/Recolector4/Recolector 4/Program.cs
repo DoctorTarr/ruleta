8911ec44325a370ea0a50b6f0e95dea7e782d5e0
HDCaro <hugo.caro@Tradehelm.com>
2020-01-27 20:27:00 -0300

    Add check to avoid run the app twice. Fix numbers table to get a more accurate reading

--- NAPSA/Recolector4/Recolector 4/Program.cs
+++ NAPSA/Recolector4/Recolector 4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DASYS.Recolector.BLL;
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace VideoRecolector
@@ -12,34 +13,45 @@ namespace VideoRecolector
         [STAThread]
         static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            try
+            bool instanceCountOne = false;
+            using (Mutex mtex = new Mutex(true, "Recolector 4", out instanceCountOne))
             {
-                foreach (string str in args)
+                if (instanceCountOne)
                 {
-                    if (str.ToLower().Contains("test"))
-                        Common.EsTest = true;
-                }
-                bool flag = Iniciador.Iniciar(Application.StartupPath);
-                try
-                {
-                    Common.Logger.EscribirLinea();
-                    Common.Logger.Escribir("*** RECOLECTOR 4 v0.1 INICIADO ***", true);
-                    Common.Logger.EscribirLinea();
-                }
-                catch
-                {
-                }
-                if (!flag)
-                    return;
-                Application.Run(new MainForm());
-            }
-            catch (Exception ex)
-            {
-                int num = (int)MessageBox.Show(ex.Message, "Error");
-            }
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    try
+                    {
+                        foreach (string str in args)
+                        {
+                            if (str.ToLower().Contains("test"))
+                                Common.EsTest = true;
+                        }
+                        bool flag = Iniciador.Iniciar(Application.StartupPath);
+                        try
+                        {
+                            Common.Logger.EscribirLinea();
+                            Common.Logger.Escribir("*** RECOLECTOR 4 v0.1 INICIADO ***", true);
+                            Common.Logger.EscribirLinea();
+                        }
+                        catch
+                        {
+                        }
+                        if (!flag)
+                            return;
+                        Application.Run(new MainForm());
+                    }
+                    catch (Exception ex)
+                    {
+                        int num = (int)MessageBox.Show(ex.Message, "Error");
+                    }
 
+                }
+                else
+                {
+                    MessageBox.Show("Ya se está ejecutando.");
+                }
+            }
         }
     }
 }
