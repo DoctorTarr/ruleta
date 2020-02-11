7ece5852364b23a22eb049079f76a7d2d1e41f85
HDCaro <hugo.caro@Tradehelm.com>
2020-02-11 15:59:10 -0300

    Change winner status also when ball is removed

--- NAPSA/Recolector4/Recolector 4/JuegoRuleta.cs
+++ NAPSA/Recolector4/Recolector 4/JuegoRuleta.cs
@@ -236,7 +236,7 @@ namespace VideoRecolector
                         break;
 
                     case WINNER_CMD_TYPE.WINNER_STATUS_CMD:
-                        if (this.contadorEstadoActual > 60)// Send the winner number cmd 65 times (aprox 30 secs)
+                        if ((this.contadorEstadoActual > 60) || (!this._isBallPresent)) // Send the winner number cmd 65 times (aprox 30 secs)
                         {
                             currentState = ESTADO_JUEGO.BEFORE_GAME; // Just in case
                             _WinnerNumberCmd = WINNER_CMD_TYPE.NO_WINNER_CMD;
